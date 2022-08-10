507a6dd5ceece08999998a9f60b07eed5d8318ba
chriscampaigne <campaignemc@gmail.com>
2022-08-10 18:58:46 -0400

    added manager prompts function

--- index.js
+++ index.js
@@ -7,3 +7,74 @@ const fs = require('fs');
 const pageTemplate = require('./dist/pageTemplate');
 
 var employees = [];
+
+const managerProfile = function () {
+    
+   return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: "What is the team manager's name?",
+            validate: name => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log('You need to enter a name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'What is the employee ID?',
+            validate: id => {
+                if (id) {
+                    return true;
+                } else {
+                    console.log('You need to enter an id!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'What is the email address of the employee?',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('You need to enter an email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'office',
+            message: 'What is the office number of the employee?',
+            validate: office => {
+                if (office) {
+                    return true;
+                } else {
+                    console.log('You need to enter an office number!');
+                    return false;
+                }
+            }
+        },
+    ])
+
+    .then(managerData => {
+        const { name, id, email, officeNumber } = managerData;
+        const manager = new Manager(name, id, email, officeNumber);
+        employees.push(manager);
+       
+      
+    })
+};
+
+
+
+
+
